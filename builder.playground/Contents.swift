9a9a48ce0bf0cab34a2d8275dab7677176321095
mfsaglam <mfsaglam1@gmail.com>
2023-05-01 21:34:49 +0300

    make an html output for list, without builder

--- builder.playground/Contents.swift
+++ builder.playground/Contents.swift
@@ -4,6 +4,14 @@ func main() {
     let hello = "hello"
     var result = "<p>\(hello)</p>"
     print(result)
+    
+    let words = ["hello", "world"]
+    result = "<ul>\n"
+    for word in words {
+        result.append("<li>\(word)</li>\n")
+    }
+    result.append("</ul>")
+    print(result)
 }
 
 main()
