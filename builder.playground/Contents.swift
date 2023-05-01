5d3f613f114c3399e5c1b061209e7b2adf331f79
mfsaglam <mfsaglam1@gmail.com>
2023-05-01 21:49:27 +0300

    add `HtlmElement`

--- builder.playground/Contents.swift
+++ builder.playground/Contents.swift
@@ -1,5 +1,44 @@
 import Foundation
 
+class HtmlElement: CustomStringConvertible {
+    var name: String = ""
+    var text: String = ""
+    var elements = [HtmlElement]()
+    
+    private let indentSize = 2
+    
+    init() {}
+    
+    init(name: String, text: String) {
+        self.name = name
+        self.text = text
+    }
+    
+    private func description(_ indent: Int) -> String {
+        var result = ""
+        
+        let i = String(repeating: " ", count: indent)
+        result += "(\(i)<\(name)>\n"
+        
+        if !text.isEmpty {
+            result += String(repeating: " ", count: indent+1)
+            result += text
+            result += "\n"
+        }
+        
+        for e in elements {
+            result += e.description(indent+1)
+        }
+        
+        result += "\(i)</\(name)>\n"
+        return result
+    }
+    
+    public var description: String {
+        return description(0)
+    }
+}
+
 func main() {
     let hello = "hello"
     var result = "<p>\(hello)</p>"
