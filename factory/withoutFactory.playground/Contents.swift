96a01241f60afb333ff8cbf37e442cf2c979be0e
mfsaglam <mfsaglam1@gmail.com>
2023-05-02 19:55:46 +0300

    move  into its own file

---
+++ factory/withoutFactory.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+class Point: CustomStringConvertible {
+    var x, y: Double
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+    
+    init(rho: Double, theta: Double) {
+        x = rho * cos(theta)
+        y = rho * sin(theta)
+    }
+    
+    static func createCartesian(x: Double, y: Double) -> Point {
+        return Point(x: x, y: y)
+    }
+    
+    static func createPolar(rho: Double, theta: Double) -> Point {
+        return Point(rho: rho, theta: theta)
+    }
+    
+    var description: String {
+        return "x = \(x), y = \(y)"
+    }
+}
+
+func main() {
+    let p = Point.createPolar(rho: 1, theta: 2)
+    print(p)
+}
+
+main()
