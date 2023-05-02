d6e7a97bebce92574f520fe51d1f7ecde9249697
mfsaglam <mfsaglam1@gmail.com>
2023-05-02 20:04:44 +0300

    make initializers private by moving the `PointFactory` class inside the `Point` class

--- factory/factory.playground/Contents.swift
+++ factory/factory.playground/Contents.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 class Point: CustomStringConvertible {
-    var x, y: Double
+    private var x, y: Double
     
-    init(x: Double, y: Double) {
+    private init(x: Double, y: Double) {
         self.x = x
         self.y = y
     }
     
-    init(rho: Double, theta: Double) {
+    private init(rho: Double, theta: Double) {
         x = rho * cos(theta)
         y = rho * sin(theta)
     }
@@ -16,20 +16,20 @@ class Point: CustomStringConvertible {
     var description: String {
         return "x = \(x), y = \(y)"
     }
-}
-
-class PointFactory {
-    static func createCartesian(x: Double, y: Double) -> Point {
-        return Point(x: x, y: y)
-    }
     
-    static func createPolar(rho: Double, theta: Double) -> Point {
-        return Point(rho: rho, theta: theta)
+    class PointFactory {
+        static func createCartesian(x: Double, y: Double) -> Point {
+            return Point(x: x, y: y)
+        }
+        
+        static func createPolar(rho: Double, theta: Double) -> Point {
+            return Point(rho: rho, theta: theta)
+        }
     }
 }
 
 func main() {
-    let p = PointFactory.createPolar(rho: 1, theta: 2)
+    let p = Point.PointFactory.createPolar(rho: 1, theta: 2)
     print(p)
 }
 
