1242dec0bbb976aabcac96c0a6b442e3aef8259a
mfsaglam <mfsaglam1@gmail.com>
2023-05-02 22:49:18 +0300

    implement the solution to increase the id every time a person is created

--- factory/exercise.playground/Contents.swift
+++ factory/exercise.playground/Contents.swift
@@ -12,7 +12,22 @@ class Person {
 }
 
 class PersonFactory {
-  func createPerson(name: String) -> Person {
-    return Person(called: name, withId: 1)
-  }
+    static var count = 0
+    
+    func createPerson(name: String) -> Person {
+        let person = Person(called: name, withId: PersonFactory.count)
+        PersonFactory.count += 1
+        return person
+    }
 }
+
+func main() {
+    let factory = PersonFactory()
+    let person1 = factory.createPerson(name: "Alice")
+    let person2 = factory.createPerson(name: "Bob")
+
+    print(person1.id)
+    print(person2.id) 
+}
+
+main()
