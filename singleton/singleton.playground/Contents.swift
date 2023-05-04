9e8a12f69e2f5826981fb181daf563858f8e4a7f
mfsaglam <mfsaglam1@gmail.com>
2023-05-04 23:41:15 +0300

    use dependency injection to inject `Database` so that we can write proper unit tests

--- singleton/singleton.playground/Contents.swift
+++ singleton/singleton.playground/Contents.swift
@@ -1,7 +1,11 @@
 import Foundation
 import XCTest
 
-class SingletonDatabase {
+protocol Database {
+    func getPopulation(_ name: String) -> Int
+}
+
+class SingletonDatabase: Database {
     var capitals = [String: Int]()
     static var instanceCount = 0
     
@@ -39,6 +43,28 @@ class SingletonRecordFinder {
     }
 }
 
+class ConfigurableRecordFinder {
+    let database: Database
+    
+    init(_ database: Database) {
+        self.database = database
+    }
+    
+    func totalPopulation(_ names: [String]) -> Int {
+        var result = 0
+        for name in names {
+            result += database.getPopulation(name)
+        }
+        return result
+    }
+}
+
+class DummyDatabase: Database {
+    func getPopulation(_ name: String) -> Int {
+        return ["alpha": 1, "beta": 2, "gamma": 3][name]!
+    }
+}
+
 func main() {
     let tests = SingletonTests()
     tests.test_singletonPopulationTest()
@@ -51,6 +77,13 @@ class SingletonTests: XCTestCase {
         let names = ["Delhi", "Mexico City"]
         let tp = rf.totalPopulation(names)
         XCTAssertEqual(143_000_000 + 174_000_000, tp, "population size must match")
+    }
+    
+    func test_dependentTotalPopulationTest() {
+        let db = DummyDatabase()
+        let names = ["alpha", "gamma"]
+        let rf = ConfigurableRecordFinder(db)
+        XCTAssertEqual(4, rf.totalPopulation(names))
     }
 }
 
