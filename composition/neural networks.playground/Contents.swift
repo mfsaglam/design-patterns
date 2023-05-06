2963f15c56ec8807a6cdcb60c7bb63223246f8b9
mfsaglam <mfsaglam1@gmail.com>
2023-05-06 22:06:09 +0300

    add extension to sequence to make `Neurons` connect to each other

--- composition/neural networks.playground/Contents.swift
+++ composition/neural networks.playground/Contents.swift
@@ -1,8 +1,12 @@
 import Foundation
 
-class Neuron {
+class Neuron: Sequence {
     var inputs = [Neuron]()
     var outputs = [Neuron]()
+    
+    func makeIterator() -> IndexingIterator<Array<Neuron>> {
+        return IndexingIterator(_elements: [self])
+    }
     
     func connect(to neuron: Neuron) {
         outputs.append(neuron)
@@ -10,11 +14,28 @@ class Neuron {
     }
 }
 
-class NeuronLayer {
+class NeuronLayer: Sequence {
     private var neurons: [Neuron]
+    
+    func makeIterator() -> IndexingIterator<Array<Neuron>> {
+        IndexingIterator(_elements: neurons)
+    }
     
     init(_ count: Int) {
         neurons = [Neuron](repeating: Neuron(), count: count)
+    }
+}
+
+extension Sequence {
+    func connect<Seq: Sequence>(to otherNeurons: Seq)
+    where Seq.Iterator.Element == Neuron,
+    Self.Iterator.Element == Neuron {
+        for neuron in self {
+            for otherNeuron in otherNeurons {
+                neuron.outputs.append(otherNeuron)
+                otherNeuron.inputs.append(neuron)
+            }
+        }
     }
 }
 
@@ -23,6 +44,11 @@ func main() {
     var neuron2 = Neuron()
     var layer1 = NeuronLayer(10)
     var layer2 = NeuronLayer(20)
+    
+    neuron1.connect(to: neuron2)
+    neuron1.connect(to: layer1)
+    layer1.connect(to: neuron1)
+    layer1.connect(to: layer2)
 }
 
 main()
