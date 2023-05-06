acb15252e6041d25eeb556e78a4f828ffc0b7ce7
mfsaglam <mfsaglam1@gmail.com>
2023-05-06 16:47:55 +0300

    staring example code

---
+++ adapter/bridge.playground/Contents.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+protocol Renderer {
+    func renderCircle(_ radius: Float)
+}
+
+class VectorRenderer: Renderer {
+    func renderCircle(_ radius: Float) {
+        print("drawing a circle of radius \(radius)")
+    }
+}
+
+class RasterRenderer: Renderer {
+    func renderCircle(_ radius: Float) {
+        print("drawing pixels for circle of radius \(radius)")
+    }
+}
+
+protocol Shape {
+    func draw()
+    func resize(_ factor: Float)
+}
+
+class Circle: Shape {
+    var radius: Float
+    var renderer: Renderer
+    
+    init(_ renderer: Renderer, _ radius: Float) {
+        self.renderer = renderer
+        self.radius = radius
+    }
+    
+    func draw() {
+        renderer.renderCircle(radius)
+    }
+    
+    func resize(_ factor: Float) {
+        radius *= factor
+    }
+}
+
+func main() {
+    let raster = RasterRenderer()
+    let vector = VectorRenderer()
+    let circle = Circle(vector, 5)
+    circle.draw()
+    circle.resize(2)
+    circle.draw()
+}
+
+main()
+
