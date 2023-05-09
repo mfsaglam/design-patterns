398c5a71dcda108f1c611bf1f48f146ed10f824a
mfsaglam <mfsaglam1@gmail.com>
2023-05-09 23:04:25 +0300

    using names efficiently

--- flyweight/repeating user names.playground/Contents.swift
+++ flyweight/repeating user names.playground/Contents.swift
@@ -12,14 +12,40 @@ class User {
     }
 }
 
+class User2 {
+    static var strings = [String]()
+    private var names = [Int]()
+    
+    init(_ fullName: String) {
+        func getOrAdd(_ s: String) -> Int {
+            if let idx = type(of: self).strings.firstIndex(of: s) {
+                return idx
+            } else {
+                type(of: self).strings.append(s)
+                return type(of: self).strings.count + 1
+            }
+        }
+        names = fullName.components(separatedBy: " ").map { getOrAdd($0) }
+    }
+    
+    static var charCount: Int {
+        return strings.map{ $0.utf8.count }.reduce(0, +)
+    }
+}
+
 func main() {
     let user1 = User("John Smith")
     let user2 = User("Jane Smith")
     let user3 = User("Jane Doe")
+    // "Smith" and "Jane" allocated twice, eat 2x memory
     
     let totalChars = user1.charCount + user2.charCount + user3.charCount
+    print("Total number of chars used: \(totalChars)")
     
-    print("Total number of chars user: \(totalChars)")
+    let user4 = User2("John Smith")
+    let user5 = User2("Jane Smith")
+    let user6 = User2("Jane Doe")
+    print("Total number of chars used: \(User2.charCount)")
 }
 
 main()
